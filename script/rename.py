11d5fbcf7b6f902a2cfbbfd8e0c7d66cb433dd68
Karan Khanna <41183388+karankhanna02@users.noreply.github.com>
2020-01-10 19:09:07 +0530

    Create rename.py

---
+++ script/rename.py
@@ -0,0 +1,7 @@
+import os
+path = 'images/'
+files = os.listdir(path)
+
+
+for index, file in enumerate(files):
+  os.rename(os.path.join(path, file), os.path.join(path, ''.join(['obj', str(index), '.jpg'])))
