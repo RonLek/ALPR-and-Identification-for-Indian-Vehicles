7202dc623113748a992b0bbc8547288b78a4b49f
Karan Khanna <41183388+karankhanna02@users.noreply.github.com>
2020-01-20 17:17:39 +0530

    Add files via upload

--- script/resize.py
+++ script/resize.py
@@ -1,13 +1,13 @@
 from PIL import Image
 import os, sys
-path = ('/home/controller/Desktop/vid2img/data/')
+path = ('/home/controller/Downloads')
 
 def resize():
   for item in os.listdir(path):
     if os.path.isfile(item):
       im = Image.open(item)
       f, e = os.path.splitext(item)
-      imResize = im.resize((480,720), Image.ANTIALIAS)
-      imResize.save('/home/controller/Desktop/vid2img/resized/' + ' resized.jpg', 'JPEG', quality=90)
+      imResize = im.resize((80,40), Image.ANTIALIAS)
+      imResize.save('/home/controller/Downloads/' + ' resized.jpg', 'JPEG', quality=90)
 
 resize()
