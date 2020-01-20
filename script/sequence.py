f8ee72fbf5bff895056bd8af1bc12167c4e89090
radheopti17 <44206584+radheopti17@users.noreply.github.com>
2020-01-20 12:26:27 +0530

    Update sequence.py

--- script/sequence.py
+++ script/sequence.py
@@ -4,4 +4,4 @@ path = os.getcwd()
 
 for i in range(967):
 	if not os.path.exists(os.path.join(path,'images', 'obj'+str(i)+'.txt')):
-    print(i,"doesn't exist")
+                print(i,"doesn't exist")
