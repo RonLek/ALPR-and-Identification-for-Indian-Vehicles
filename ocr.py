9ec7036b4f28fc6d75c4cfcad9ade6cfa0243189
Rohan Lekhwani <rohanlekhwani@ymail.com>
2020-02-07 20:23:34 +0530

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -1,7 +1,38 @@
+states = ['AP', 'AR', 'AS', 'BR', 'CG', 'GA',
+          'GJ', 'HR', 'HP', 'JH', 'KA', 'KL',
+          'MP', 'MH', 'MN', 'ML', 'MZ', 'NL',
+          'OD', 'PB', 'RJ', 'SK', 'TN', 'TS',
+          'TR', 'UP', 'UK', 'WB', 'AN', 'CH',
+          'DD', 'DL', 'JK', 'LA', 'LD', 'PY']
+
+def resultplate(plate):
+    result=""
+    j=0
+    for character in plate:
+        if character.isalnum():
+            result+=character
+        if character.isdigit():
+            j+=1
+        else:
+            j=0
+        if j==4:
+            break
+    if j!=4:
+        print('Couldn\'t extract number')
+    else:
+        while result[0:2] not in states and result!="":
+            result=result[2:]
+        if result=="":
+            print('Couldn\'t Recognize Plate. Try with a different plate')
+        else:
+            return result
+
 def preprocess(plate):
-    plate.strip('IND')
-    plate.strip('\n')
-    print(plate)
+    plate = plate.replace('\n', '')
+    plate = plate.replace('INDIA', '')
+    plate = plate.replace('IND', '')
+    plate = plate.replace('IN', '')
+    return plate
     
 def detect_text(path):
     """Detects text in the file."""
@@ -16,20 +47,21 @@ def detect_text(path):
 
     response = client.text_detection(image=image)
     texts = response.text_annotations
-    print('Texts:')
-
-    with open('results.txt', 'w', encoding='utf8') as f:
-        result=""
-        for text in texts:
-            result+=text.description
+    #with open('results.txt', 'w', encoding='utf8') as f:
+        #result=""
+        #for text in texts:
+        #    result+=text.description
          #   result+='\n"{}"'.format(text.description)
 
             #vertices = (['({},{})'.format(vertex.x, vertex.y)
              #           for vertex in text.bounding_poly.vertices])
 
             #result+='bounds: {}'.format(','.join(vertices))
-        preprocess(result)
-        f.write(result)
+        #f.write(result)
+    plate = preprocess(texts[0].description)
+    plate = resultplate(plate)
+    print(plate)
+        
 
     if response.error.message:
         raise Exception(
